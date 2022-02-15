139d6446a31698d87ffe0b5ec619c3f37b1555d9
quark <>
2022-02-15 20:27:12 +0500

    commut 2 - tester

--- .idea/src/java/tester/commitsTester.java
+++ .idea/src/java/tester/commitsTester.java
@@ -3,5 +3,6 @@ package tester;
 public class commitsTester {
     public commitsTester() {
         System.out.println("commit 1 - tester");
+        System.out.println("commit 2 - tester");
     }
 }
